ef8da4fedd87e63cc6dfc0012ad45d665c4d30b5
Gephery <grucemax@uw.edu>
2017-08-07 10:47:17 -0400

    Updated lots of stuff.

---
+++ src/events/test_plugin/CMakeFiles/test_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_plugin.dir/Users/gephery/github/code-name/src/events/info_manager.cpp.o"
+  "CMakeFiles/test_plugin.dir/Users/gephery/github/code-name/src/events/event.cpp.o"
+  "CMakeFiles/test_plugin.dir/main.cpp.o"
+  "build/test_plugin.pdb"
+  "build/test_plugin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
